08daf84d2b31b8c4e93069d967142f389bb50f3a
phacf <phacff@gmail.com>
2021-07-12 10:38:30 -0300

    CONFIGS

--- src/util/config.ts
+++ src/util/config.ts
@@ -5,5 +5,7 @@ dotenv.config();
 export const SERVER_CONFIG = {
     SERVER_PORT: process.env.SERVER_PORT,
     SERVER_HOST: process.env.SERVER_HOST,
+    SERVER_USER: process.env.SERVER_USER,
+    SERVER_PASS: process.env.SERVER_PASSWORD,
 }
 
